a24cdf04bbdc40e1c7732931a0f92c0957480035
github username <github email address>
2018-02-05 08:39:16 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,27 @@
 // declare your functions here...
+
+//
+// Write a function divSelector that does not accept any parameters. The function should use a class selector to return the two divs with the class pics.
+//
+// Write a function firstListItem that does not accept any parameters. The function should use a first-child selector to return the first list item in the ul with the ID pic-list.
+
+function paragraphSelector() {
+  return $("p");
+}
+
+function lastImageSelector() {
+  return $("img:last");
+}
+
+function ninjaBabySelector() {
+  return $("#baby-ninja");
+}
+
+function divSelector() {
+  return $(".pics");
+}
+
+function firstListItem() {
+  return $("parent-tag child-tag:first-child");
+  return $("div ul:pic-list");
+}
